3c2473a94c7302c2206db432f1794aa860bdc327
Boby Siddana <bobys@gmail.com>
2025-07-19 18:07:46 +0530

    hello world started

--- 05-datatypes.sh
+++ 05-datatypes.sh
@@ -6,4 +6,4 @@ Number2=200
 TIMESTAMP=$(date)
 echo "script executed at: $TIMESTAMP"
 SUM=$(($Number1=$Number2))
-echo "SUMof $Number1 and $Number2 is: $SUM"
+echo "SUM of $Number1 and $Number2 is: $SUM"
