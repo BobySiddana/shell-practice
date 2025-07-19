7d831ea9dd629c77ac4a344e34aa4974b4990a36
Boby Siddana <bobys@gmail.com>
2025-07-19 17:08:42 +0530

    hello world started

---
+++ 03-varaibles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=Alex
+person2=Ben
+
+echo "person1: Hey Ben, how was your day?"
+echo "person2: Pretty good, pretty good. Just finished up work, you?"
+echo "person1: Yeah, not bad. Got through some emails. Any plans for tonight?"
+echo "person2: Nah, just chilling. Might try that new pizza place."
