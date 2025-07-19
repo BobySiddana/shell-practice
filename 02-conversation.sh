f9aea1b8994a685d634c5ced487cc10f268c10e1
Boby Siddana <bobys@gmail.com>
2025-07-19 17:50:25 +0530

    hello world started

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo "Alex: Hey Ben, how was your day?"
+echo "Ben: Pretty good, pretty good. Just finished up work, you?"
+echo "Alex: Yeah, not bad. Got through some emails. Any plans for tonight?"
+echo "Ben: Nah, just chilling. Might try that new pizza place."
