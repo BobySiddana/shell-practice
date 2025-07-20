ba20a1ac0cc5df4fac09cf860f33cead17da6752
Boby Siddana <bobys@gmail.com>
2025-07-20 17:48:09 +0530

    logs

--- 13-logs.sh
+++ 13-logs.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" 
@@ -14,46 +10,44 @@ echo "script started executing at: $(date)" &>>$LOG_FILE
 
 if [ $USERID -ne 0 ]
 then
-   echo "ERROR:: please run with root access" &>>$LOG_FILE
+   echo "ERROR:: please run with root access"
    EXIT 1
 else
-   echo "you are running with root access" &>>$LOG_FILE
+   echo "you are running with root access"
 fi
 VALIDATE(){
     if [ $1 -eq 0 ]
 then 
-    echo "installing $2 is ...SUCCESS" &>>$LOG_FILE
+    echo "installing $2 is ...SUCCESS"
 else
-    echo "installing is $2 ...FAILURE" &>>$LOG_FILE
+    echo "installing is $2 ...FAILURE"
     EXIT 1
 fi
 }
-dnf list installed mysql &>>$LOG_FILE
+dnf list installed mysql
 if [ $? -ne 0 ]
 then
-    echo "mysql is not installed.. going to be istalled" &>>$LOG_FILE
-    dnf install mysql -y &>>$LOG_FILE
+    echo "mysql is not installed.. going to be istalled"
+    dnf install mysql -y
     VALIDATE $? "mysql"
   else
-      echo "mysql is already insalled.. noting to do" &>>$LOG_FILE
+      echo "mysql is already insalled.. noting to do":
 fi
 dnf list installed pyton3
 if [ $? -ne 0 ]
 then
-    echo "python3 is not installed.. going to be istalled" &>>$LOG_FILE
-    dnf install python3 -y &>>$LOG_FILE
+    echo "python3 is not installed.. going to be istalled"
+    dnf install python3 -y
     VALIDATE $? "python3"
   else
-      echo "python3 is already insalled.. noting to do" &>>$LOG_FILE
+      echo "python3 is already insalled.. noting to do":
 fi
-dnf list installed nginx &>>$LOG_FILE
+dnf list installed nginx
 if [ $? -ne 0 ]
 then
-    echo "nginx is not installed.. going to be istalled" &>>$LOG_FILE
-    dnf install nginx -y &>>$LOG_FILE
+    echo "nginx is not installed.. going to be istalled"
+    dnf install nginx -y
     VALIDATE $? "nginx"
   else
-      echo "nginx is already insalled.. noting to do" &>>$LOG_FILE
+      echo "nginx is already insalled.. noting to do":
 fi
-
-
