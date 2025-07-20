72dbcb9ffc53cec86c8778c88ef2798e80cc91b2
Boby Siddana <bobys@gmail.com>
2025-07-20 18:11:31 +0530

    logs

--- 13-logs.sh
+++ 13-logs.sh
@@ -4,6 +4,7 @@ USERID=$(id -u)
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" 
+PACKAGES=("mysql" "python" "nginx")
 
 mkdir -p $LOGS_FOLDER
 echo "script started executing at: $(date)" &>>$LOG_FILE
@@ -24,31 +25,15 @@ else
     EXIT 1
 fi
 }
-dnf list installed mysql &>>$LOG_FILE
+for package in ${PACKAGES[@]}
+do
+     dnf list installed $package &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
-    echo "mysql is not installed.. going to be istalled" | tee -a $LOG_FILE
-    dnf install mysql -y &>>$LOG_FILE
-    VALIDATE $? "mysql"
-  else
-      echo "mysql is already insalled.. noting to do" | tee -a $LOG_FILE
+    echo "$package is not installed.. going to be istalled" | tee -a $LOG_FILE
+    dnf install $package -y &>>$LOG_FILE
+    VALIDATE $? "$package"
+else
+    echo "$package is already insalled.. noting to do" | tee -a $LOG_FILE
 fi
-dnf list installed pyton3 &>>$LOG_FILE
-if [ $? -ne 0 ]
-then
-    echo "python3 is not installed.. going to be istalled" | tee -a $LOG_FILE
-    dnf install python3 -y &>>$LOG_FILE
-    VALIDATE $? "python3"
-  else
-      echo "python3 is already insalled.. noting to do" | tee -a $LOG_FILE
-fi
-dnf list installed nginx &>>$LOG_FILE
-if [ $? -ne 0 ]
-then
-    echo "nginx is not installed.. going to be istalled" | tee -a $LOG_FILE
-    dnf install nginx -y &>>$LOG_FILE
-    VALIDATE $? "nginx"
-  else
-    echo "nginx is not installed.. going to be istalled" | tee -a $LOG_FILE
-      echo "nginx is already insalled.. noting to do": 
-fi
+done
