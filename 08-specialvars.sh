53ed1cc309ee36209583ac33f86160acfe8085fc
Boby Siddana <bobys@gmail.com>
2025-07-20 06:59:31 +0530

    hello world started

--- 08-specialvars.sh
+++ 08-specialvars.sh
@@ -2,3 +2,9 @@
 
 echo "All variables passed to the script: $@"
 echo "Number of varaibles: $#"
+echo "present working directory: $PWD"
+echo "user running this script: $USER"
+echo "Home directory of the user: $HOME"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of last command in background: $!"
